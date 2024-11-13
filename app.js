94a60bebeb10dde7f7cc4eafcf4721354bcadeab
Dylan <quintarddylan@gmail.com>
2024-11-13 13:38:30 +0100

    second commit

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const catsRoutes  = require('./routes/cats')
 const path = require('path');
 
-mongoose.connect('mongodb+srv://eazybang1331:olxAk6oOfb3ESzCA@cattery.2rmjy.mongodb.net/?retryWrites=true&w=majority',
-    )
+mongoose.connect(process.env.MONGODB_URI)  // Utiliser la variable d'environnement
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch((error) => console.log('Connexion à MongoDB échouée !', error));
 
 const app = express();
 
